ac2beb5a2c4c154d3eede314273c1b646b349fe7
vinitcoder05 <vdalal22cs@student.mes.ac.in>
2025-01-25 15:11:18 +0000

    dynamic database

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
 import os
 
-db_connection_string = os.environ["DB_CONNECTION_STRING"]
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
@@ -20,12 +20,13 @@ def load_jobs_from_db():
 
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), val=id)
-    rows = result.all()
-    if len(rows) == 0:
-      return None
-    else:
-      return dict(rows[0])
+    result = conn.execute(text("select * from jobs"))
+
+    result_dicts = []
+    for row in result.all():
+      result_dicts.append(dict(row._mapping))
+
+    print(result_dicts)
 
 
 def add_application_to_db(job_id, data):
@@ -34,11 +35,8 @@ def add_application_to_db(job_id, data):
         "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
     )
 
-    conn.execute(query,
-                 job_id=job_id,
-                 full_name=data['full_name'],
-                 email=data['email'],
-                 linkedin_url=data['linkedin_url'],
-                 education=data['education'],
-                 work_experience=data['work_experience'],
-                 resume_url=data['resume_url'])
+    query = text(
+        f"INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES ({job_id}, '{data['full_name']}', '{data['email']}', '{data['linkedin_url']}', '{data['education']}', '{data['work_experience']}', '{data['resume_url']}')"
+    )
+
+    conn.execute(query)
